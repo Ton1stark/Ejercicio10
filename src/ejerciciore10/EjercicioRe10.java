31131a9bf4c72af245c4aa243400c61a60259611
Ton1stark <ccastanedab@unal.edu.co>
2025-01-14 21:30:57 -0500

    EjercicioRe10.java

--- src/ejerciciore10/EjercicioRe10.java
+++ src/ejerciciore10/EjercicioRe10.java
@@ -4,15 +4,8 @@
  */
 package ejerciciore10;
 
-/**
- *
- * @author Dell
- */
 public class EjercicioRe10 {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
       VentanaPrincipal p = new VentanaPrincipal();
        p.setVisible(true);
